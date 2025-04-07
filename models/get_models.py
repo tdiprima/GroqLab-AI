112f7987ee7f7574332a2a3306a26e332ab5eb20
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-04-07 15:05:01 -0400

    get available models

---
+++ models/get_models.py
@@ -0,0 +1,10 @@
+import os
+from groq import Groq
+
+# Initialize the client with your API key
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+# List all available models
+models = client.models.list()
+for model in models.data:
+    print(model.id)
