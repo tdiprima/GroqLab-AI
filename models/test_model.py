7fcb65f82426ae56fc2e9cf71525fcb2234c607b
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-04-08 10:24:17 -0400

    test model

---
+++ models/test_model.py
@@ -0,0 +1,12 @@
+import os
+from groq import Groq
+
+# Initialize the client with your API key
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+response = client.chat.completions.create(
+    messages=[{"role": "user", "content": "Hello! Who are you?"}],
+    # model="llama3-70b-8192"
+    model="meta-llama/llama-4-scout-17b-16e-instruct"
+)
+print(response.choices[0].message.content)
